d8de1dc34bae088d75e86f6b1e5d1910f1ed7434
Kieran Connell <khconnell@gmail.com>
2026-04-03 23:48:28 +0200

    Rebuild with new Beebasm.

--- make.bat
+++ make.bat
@@ -1,3 +1,3 @@
-@..\..\Bin\beebasm.exe -i thrust.6502 -do thrust.ssd -boot Thrust -v
+@.\beebasm.exe -i thrust.6502 -do thrust.ssd -boot Thrust -v > compile.txt
 @.\crc32dos.exe thrust.ssd
 @echo 6389c446
